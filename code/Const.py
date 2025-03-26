49006365f2969a61d2b16a2aae32127dd9c1a6e0
LauraG <laura.guimaraes.souza@hotmail.com>
2025-03-26 02:01:54 -0300

    Almost done must fix - score and level 2 background

--- code/Const.py
+++ code/Const.py
@@ -4,6 +4,7 @@ import pygame
 C_ORANGE = (252, 92, 38)
 C_WHITE = (255, 255, 255)
 C_YELLOW = (252, 202, 38)
+C_GREEN = (0, 128, 0)
 
 # E
 EVENT_ENEMY = pygame.USEREVENT + 1
@@ -19,6 +20,7 @@ ENTITY_SPEED = {
     'Level2Bg2': 2,
     'Level2Bg3': 3,
     'Level2Bg4': 3,
+    'PlayerShot': 2,
     'Obst1': 3,
     'Obst2': 3,
     'Obst3': 3,
@@ -42,10 +44,52 @@ ENTITY_HEALTH = {
     'Obst3': 1,
     'Obst4': 2,
 }
+
+ENTITY_DAMAGE = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 0,
+    'Level1Bg2': 0,
+    'Level1Bg3': 0,
+    'Level2Bg0': 0,
+    'Level2Bg1': 0,
+    'Level2Bg2': 0,
+    'Level2Bg3': 0,
+    'Level2Bg4': 0,
+    'Player': 0,
+    'PlayerShot': 1,
+    'Obst1': 20,
+    'Obst2': 25,
+    'Obst3': 20,
+    'Obst4': 25,
+}
+
+ENTITY_SCORE = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 0,
+    'Level1Bg2': 0,
+    'Level1Bg3': 0,
+    'Level2Bg0': 0,
+    'Level2Bg1': 0,
+    'Level2Bg2': 0,
+    'Level2Bg3': 0,
+    'Level2Bg4': 0,
+    'Player': 0,
+    'PlayerShot': 0,
+    'Obst1': 100,
+    'Obst2': 125,
+    'Obst3': 100,
+    'Obst4': 125,
+}
+
+ENTITY_SHOT_DELAY = {'Player': 20}
+
 # M
 MENU_OPTION = ('NEW GAME',
                'HIGH SCORE',
                'EXIT')
+
+# P
+PLAYER_KEY_SHOOT = {'Player': pygame.K_RSHIFT}
 
 # S
 SPAWN_TIME = 4000
@@ -57,3 +101,20 @@ TIMEOUT_LEVEL = 20000
 # W
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
+
+# S
+SCORE_POS = {'Title': (WIN_WIDTH / 2, 50),
+             'EnterName': (WIN_WIDTH / 2, 80),
+             'Label': (WIN_WIDTH / 2, 90),
+             'Name': (WIN_WIDTH / 2, 110),
+             0: (WIN_WIDTH / 2, 110),
+             1: (WIN_WIDTH / 2, 130),
+             2: (WIN_WIDTH / 2, 150),
+             3: (WIN_WIDTH / 2, 170),
+             4: (WIN_WIDTH / 2, 190),
+             5: (WIN_WIDTH / 2, 210),
+             6: (WIN_WIDTH / 2, 230),
+             7: (WIN_WIDTH / 2, 250),
+             8: (WIN_WIDTH / 2, 270),
+             9: (WIN_WIDTH / 2, 290),
+             }
--- code/Player.py
+++ code/Player.py
@@ -2,12 +2,15 @@
 # -*- coding: utf-8 -*-
 import pygame.key
 
+from code.Const import PLAYER_KEY_SHOOT, ENTITY_SHOT_DELAY
 from code.Entity import Entity
+from code.PlayerShot import PlayerShot
 
 
 class Player(Entity):
     def __init__(self, name: str, position: tuple):
         super().__init__(name, position)
+        self.shot_delay = ENTITY_SHOT_DELAY[self.name]
 
 
     def update(self):
@@ -15,3 +18,15 @@ class Player(Entity):
 
     def move(self):
         pass
+
+    def shoot(self):
+        self.shot_delay -= 1
+        if self.shot_delay == 0:
+            self.shot_delay = ENTITY_SHOT_DELAY[self.name]
+            pressed_key = pygame.key.get_pressed()
+            if pressed_key[PLAYER_KEY_SHOOT[self.name]]:
+                return PlayerShot(name=f'{self.name}Shot', position=(self.rect.centerx, self.rect.centery))
+            else:
+                return None
+        else:
+            return None
